7673a4d7b1cb947a96227f2090b72a7aa5e7ad5d
Your Name <you@example.com>
2024-06-21 19:04:49 +0200

    db fix

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //! DISATTIVATO: app.use("/auth", userAuth);
-
 app.use('/posts', postsRouter);
 app.use('/categories', categoriesRouter);
 app.use('/tags', tagsRouter);
